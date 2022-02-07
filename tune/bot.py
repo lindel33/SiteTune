d593fcc72f74fad4936b43de9f81f7864f8c4cec
luky <lindel33@mail.ru>
2022-02-07 22:39:59 +0300

    Вроде работает

--- tune/bot.py
+++ tune/bot.py
@@ -1,89 +1,42 @@
-# -*- coding: utf-8 -*-
-import time
-from pprint import pprint
-import telebot
-from data import get_category, get_products, get_detail_product
+import asyncio
+import aiohttp
+from aiohttp import web
+import json
 
-API_KEY = 'https://api.telegram.org/bot5292093759:AAHrxxCOr58zIQzmzaxZ98-IYucJE4pmFWs/'
-token = '5292093759:AAHrxxCOr58zIQzmzaxZ98-IYucJE4pmFWs'
-client = telebot.TeleBot(token)
-chat_id = -1001392557374
+TOKEN = '111111111:AAHKeYAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN
 
-category = get_category()
-category_all = [x for x in category]
-categories = [[x[1]] for x in category]
-category_list = [x[1] for x in category]
+async def handler(request):
+    data = await request.json()
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    message = {
+        'chat_id': data['message']['chat']['id'],
+        'text': data['message']['text']
+    }
+    async with aiohttp.ClientSession(loop=loop) as session:
+        async with session.post(API_URL,
+                                data=json.dumps(message),
+                                headers=headers) as resp:
+            try:
+                assert resp.status == 200
+            except:
+                return web.Response(status=500)
+    return web.Response(status=200)
 
+async def init_app(loop):
+    app = web.Application(loop=loop, middlewares=[])
+    app.router.add_post('/api/v1', handler)
+    return app
 
-@client.message_handler(commands=['start'])
-def start_message(message):
-    keyboard_category = telebot.types.ReplyKeyboardMarkup(True, True)
-    keyboard_category.keyboard = categories
-    client.send_message(chat_id=message.chat.id,
-                        text='Что хотите найти?',
-                        reply_markup=keyboard_category)
-
-
-@client.message_handler(content_types=['text'])
-def get_all_product(message):
-    if message.text in category_list:
-        idc = [x[0] for x in category_all if x[1] == message.text][0]
-        products = [[x[0]] for x in get_products(idc)]
-        if products:
-            keyboard_products = telebot.types.ReplyKeyboardMarkup(True, True)
-            keyboard_products.keyboard = products
-            client.send_message(chat_id=message.chat.id,
-                                text='Ищу: ' + message.text,
-                                reply_markup=keyboard_products)
-            return 0
-        else:
-            time.sleep(2)
-            keyboard_products = telebot.types.ReplyKeyboardMarkup(True, True)
-            keyboard_products.keyboard = products
-            client.send_message(chat_id=message.chat.id,
-                                text='Сейчас в этой категории пусто 😔 ',
-                                reply_markup=keyboard_products)
-    else:
-        try:
-            detail_product = get_detail_product(message.text)
-
-            if detail_product[1] != 'default.jpg':
-                f1 = open(detail_product[1], 'rb')
-                f1 = f1.read()
-                f2 = open(detail_product[2], 'rb')
-                f2 = f2.read()
-                f3 = open(detail_product[3], 'rb')
-                f3 = f3.read()
-            else:
-                f1, f2, f3 = open('default.jpg', 'rb'), open('default.jpg', 'rb'), open('default.jpg', 'rb')
-                f1, f2, f3 = f1.read(), f2.read(), f3.read()
-            client.send_media_group(chat_id=message.chat.id, media=[
-            telebot.types.InputMediaPhoto(f1, caption=detail_product[8]),
-            telebot.types.InputMediaPhoto(f2),
-            telebot.types.InputMediaPhoto(f3),
-
-            ])
-        except:
-            client.send_message(chat_id=message.chat.id,
-                                text='Не нашел ничего 🙄',
-                                )
-            keyboard_category = telebot.types.ReplyKeyboardMarkup(True, True)
-            keyboard_category.keyboard = categories
-            client.send_message(chat_id=message.chat.id,
-                                text='Найдём что-то другое? 🙃',
-                                reply_markup=keyboard_category)
-            return 0
-
-        keyboard_category = telebot.types.ReplyKeyboardMarkup(True, True)
-        keyboard_category.keyboard = categories
-        client.send_message(chat_id=message.chat.id,
-                            text='Что ещё посмотрим? 🤗',
-                            reply_markup=keyboard_category)
-        return 0
-    keyboard_category = telebot.types.ReplyKeyboardMarkup(True, True)
-    keyboard_category.keyboard = categories
-    client.send_message(chat_id=message.chat.id,
-                        text='Что ещё посмотрим? ',
-                        reply_markup=keyboard_category)
-
-client.polling(none_stop=True)
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        app = loop.run_until_complete(init_app(loop))
+        web.run_app(app, host='0.0.0.0', port=23456)
+    except Exception as e:
+        print('Error create server: %r' % e)
+    finally:
+        pass
+    loop.close()
